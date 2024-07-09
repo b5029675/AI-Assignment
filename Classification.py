ba3127afd5bfef8d3af6ddf1b5a5734a1a524a72
b5029675 <b5029675@my.shu.ac.uk>
2024-07-09 17:44:15 +0000

    commit

--- Classification.py
+++ Classification.py
@@ -4,10 +4,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 #KNN CLASSIFICATION
 
@@ -39,33 +38,6 @@ scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
-
-
-#Creating graph for optimal K
-# creating list of K for KNN
-k_list = list(range(1,50,2))
-# creating list of cv scores
-cv_scores = []
-
-# perform 10-fold cross validation
-for k in k_list:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
-    cv_scores.append(scores.mean())
-
-# changing to misclassification error
-MSE = [1 - x for x in cv_scores]
-
-
-plt.figure()
-plt.figure(figsize=(15,10))
-plt.title('The optimal number of neighbors', fontsize=20, fontweight='bold')
-plt.xlabel('Number of Neighbors K', fontsize=15)
-plt.ylabel('Misclassification Error', fontsize=15)
-sns.set_style("whitegrid")
-plt.plot(k_list, MSE)
-
-
 
 
 #Create KNN classifier
