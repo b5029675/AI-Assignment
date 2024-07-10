a6717cfe754a3c8a8a53fb8b4a3a07eb10b9a532
b5029675 <b5029675@my.shu.ac.uk>
2024-07-10 20:17:59 +0000

    final

--- Clustering.py
+++ Clustering.py
@@ -8,7 +8,7 @@ from sklearn.metrics import silhouette_samples, silhouette_score
 data_url = "Mall_Customers.csv"
 dataset = pd.read_csv(data_url)
 
-X = dataset.iloc[:, [1, 2, 3, 4]].values
+X = dataset.iloc[:, [2, 3, 4]].values
 
 
 #The Elbow Method
@@ -29,6 +29,20 @@ plt.xlabel('Number of clusters')
 plt.ylabel('WCSS') #within cluster sum of squares
 plt.show()
 
+kmeans = KMeans(n_clusters = 6, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'purple', label = 'Cluster4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'orange', label = 'Cluster5')
+plt.scatter(X[y_kmeans == 5, 0], X[y_kmeans == 5, 1], s = 100, c = 'pink', label = 'Cluster6')
+
+#Plotting the centroids of the clusters
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
 
 
 #Silhouette Method
@@ -37,7 +51,7 @@ plt.show()
 # This particular setting has one distinct cluster and 3 clusters placed close
 # together.
 
-range_n_clusters = [2, 3, 4, 5, 6, 7, 8]
+range_n_clusters = [2, 3, 4, 5, 6]
 
 for n_clusters in range_n_clusters:
     # Create a subplot with 1 row and 2 columns
